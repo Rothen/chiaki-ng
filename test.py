3c192cabd338dca01299acc2e29459d7c02cb093
Benjamin Ricchiuto <beni.ricchiuto@gmail.com>
2025-03-14 16:15:49 +0100

    Refactor morning variable to use a list of integers and clean up StreamSession event handling code

--- test.py
+++ test.py
@@ -6,9 +6,9 @@ regist_key = "b02d1ceb"  # 2955746539
 nickname = "PS5-083"
 ps5Id = "78c881a8214a"
 morning = "ª?RÿGC\\x1d/,ðñA\\x10öy³"
-morning_x = 0x000001b142182250
+morning_ints: list[int] = [170, 63, 82, 255, 71, 67, 29, 47, 44, 240, 241, 65, 16, 246, 121, 179]
 initial_login_pin = ""  # None
-duid = "" # None
+duid = ""  # None
 auto_regist = False
 fullscreen = False
 zoom = False
@@ -41,20 +41,8 @@ connect_info: StreamSessionConnectInfo = StreamSessionConnectInfo(
 print(connect_info)
 
 stream_session: StreamSession = StreamSession(connect_info)
+
 print(stream_session)
-stream_session.ffmpeg_frame_available = lambda : print('ffmpeg_frame_available')
-stream_session.session_quit = lambda a, b: print('session_quit')
-stream_session.login_pin_requested = lambda a: print('login_pin_requested')
-stream_session.data_holepunch_progress = lambda a: print('data_holepunch_progress')
-stream_session.auto_regist_succeeded = lambda a: print('auto_regist_succeeded')
-stream_session.nickname_received = lambda a: print('nickname_received')
-stream_session.connected_changed = lambda : print('connected_changed')
-stream_session.measured_bitrate_changed = lambda : print('measured_bitrate_changed')
-stream_session.average_packet_loss_changed = lambda : print('average_packet_loss_changed')
-stream_session.cant_display_changed = lambda a: print('cant_display_changed')
-print(stream_session.start())
-
-
 # log.set_callback(lambda level, message: print(f"[{level}] {message}"))
 # print(wakeup(log, host, registKey, ps5))
 # print(discover(log, host, discover_timout))
